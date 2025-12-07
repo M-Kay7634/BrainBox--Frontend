e6ca63de4af316d034a92f9d54a5e89fad9d67fc
M-Kay7634 <manishlloyd2807@gmail.com>
2025-12-07 21:07:37 +0530

    dashboard.jsx -> Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Dashboard from "./pages/Dashboard.jsx";
+import Dashboard from "./pages/Dashboard";
 import MemoryGame from "./games/MemoryGame";
 import SpeedMath from "./games/SpeedMath";
 import Result from "./pages/Result";
